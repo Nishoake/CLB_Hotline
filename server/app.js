10b258c740e49963daa2e7dcb38a993c5e61386d
Nishoake <nsribava@uwaterloo.ca>
2021-01-19 12:27:18 -0500

    DB query linked to keyword from request body

--- server/app.js
+++ server/app.js
@@ -148,9 +148,9 @@ app.post('/bling', async (request, response) => {
 
   if(user){
     if (request.body.Body === 'TAKECARE'){
-      // let result = await Subscriber.findByIdAndDelete(user._id)
+      let result = await Subscriber.findByIdAndDelete(user._id)
 
-      // console.log(`result: ${result}`)
+      console.log(`result: ${result}`)
       twiml.message(`Last Name Ever; First Name Greatest ⭐️`)
 
       // twiml.message(`${user.name} you have successfully been unsubscribed from the C.L.B. Hotline. You will not receive any more messages from this number.`)
