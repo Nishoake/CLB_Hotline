b10de9ec63f5b04efc40fc16c7821844c84d7285
Nishoake <nsribava@uwaterloo.ca>
2021-02-16 03:52:24 -0500

    Rebuilt UI to the server

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c63be6324de5ba7eb777389ed19f61d8.js"
+  "/precache-manifest.8893975a8a0356eb4a59bf530d1dae44.js"
 );
 
 self.addEventListener('message', (event) => {
--- server/build/precache-manifest.8893975a8a0356eb4a59bf530d1dae44.js
+++ server/build/precache-manifest.8893975a8a0356eb4a59bf530d1dae44.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fb305b54ed4b592e32371860c6dd0524",
+    "revision": "413ca434f94d2f08a9ae7a6cca356c13",
     "url": "/index.html"
   },
   {
-    "revision": "146a7ea8abd8b7395cbe",
-    "url": "/static/css/main.d83da3eb.chunk.css"
+    "revision": "49953f4dbce44f1f9f94",
+    "url": "/static/css/main.dce83399.chunk.css"
   },
   {
     "revision": "cf84648cc6b4feb69668",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.0b38f417.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "146a7ea8abd8b7395cbe",
-    "url": "/static/js/main.8f01591e.chunk.js"
+    "revision": "49953f4dbce44f1f9f94",
+    "url": "/static/js/main.e38f2cc2.chunk.js"
   },
   {
     "revision": "8cc2723afdfdd86517d9",
